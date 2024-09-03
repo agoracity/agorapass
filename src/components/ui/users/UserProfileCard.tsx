e082ad0ce83b9ee819c0b15455d97ac9577bd75c
Matias <marazza.matias@gmail.com>
2024-09-03 19:56:17 -0300

    Show loading in user card

--- src/components/ui/users/UserProfileCard.tsx
+++ src/components/ui/users/UserProfileCard.tsx
@@ -20,10 +20,12 @@ const UserProfileCard: React.FC<UserProfileCardProps> = ({ recipient, onVouch, o
     const [vouchesMade, setVouchesMade] = useState<number | null>(null);
     const [vouchesReceived, setVouchesReceived] = useState<number | null>(null);
     const [ensName, setEnsName] = useState<string | null>(null);
+    const [isLoading, setIsLoading] = useState(true);
     const schemaId = process.env.NEXT_PUBLIC_SCHEMA_ID!;
 
     useEffect(() => {
         const fetchData = async () => {
+            setIsLoading(true);
             try {
                 const [made, received, ensNames] = await Promise.all([
                     fetchAttestationsMadeCount(schemaId, recipient),
@@ -35,6 +37,8 @@ const UserProfileCard: React.FC<UserProfileCardProps> = ({ recipient, onVouch, o
                 setEnsName(ensNames.length > 0 ? ensNames : null);
             } catch (error) {
                 console.error('Error fetching data:', error);
+            } finally {
+                setIsLoading(false);
             }
         };
 
@@ -45,80 +49,86 @@ const UserProfileCard: React.FC<UserProfileCardProps> = ({ recipient, onVouch, o
     console.log(userData, 'userData');
     return (
         <div className="w-full rounded-xl p-6 bg-white">
-            <div className="flex flex-col lg:flex-row gap-6">
-            <div className="flex-shrink-0">
-                <Avatar className="w-24 h-24 mx-auto lg:mx-0">
-                    {avatar && (
-                        typeof avatar === 'string' ? (
-                            <AvatarImage src={avatar} alt="Avatar" />
-                        ) : (
-                            avatar
-                        )
-                    )}
-                </Avatar>
-            </div>
-
-            <div className="flex-grow">
-                <div className="text-center lg:text-left">
-                    <h2 className="text-2xl font-bold text-zinc-700">{ensName || truncateWallet(recipient)}</h2>
-                    {ensName && <p className="mt-2 font-semibold text-zinc-700">{truncateWallet(recipient)}</p>}
-                    {/* <p className="mt-2 font-semibold text-zinc-700">@{(ensName || truncateWallet(recipient)).toLowerCase()}</p> */}
-                    
-                    {userData?.name && <p className="mt-2 text-zinc-600">{userData.name}</p>}
-                    
-                    {userData?.bio && <p className="mt-2 text-zinc-500">{userData.bio}</p>}
-                    
-                    {userData?.createdAt && (
-                        <p className="mt-2 text-sm text-zinc-400">
-                            Member since: {new Date(userData.createdAt).toLocaleDateString()}
-                        </p>
-                    )}
-                    
-                    <div className="mt-2 flex justify-center lg:justify-start space-x-4">
-                        {userData?.twitter && (
-                            <a href={`https://twitter.com/${userData.twitter}`} target="_blank" rel="noopener noreferrer" className="flex items-center text-sky-400">
-                                <Image src={TwitterLogo} alt="Twitter" className="w-5 h-5 mr-1" />
-                                @{userData.twitter}
-                            </a>
-                        )}
-                        {userData?.farcaster && (
-                            <a href={`https://warpcast.com/${userData.farcaster}`} target="_blank" rel="noopener noreferrer" className="flex items-center text-[#8a63d2]">
-                                <Image src={FarcasterLogo} alt="Farcaster" className="w-5 h-5 mr-1" />
-                                @{userData.farcaster}
-                            </a>
-                        )}
+            {isLoading ? (
+                <div className="flex justify-center items-center h-64">
+                    <p className="text-lg font-semibold text-zinc-600">Loading...</p>
+                </div>
+            ) : (
+                <div className="flex flex-col lg:flex-row gap-6">
+                    <div className="flex-shrink-0">
+                        <Avatar className="w-24 h-24 mx-auto lg:mx-0">
+                            {avatar && (
+                                typeof avatar === 'string' ? (
+                                    <AvatarImage src={avatar} alt="Avatar" />
+                                ) : (
+                                    avatar
+                                )
+                            )}
+                        </Avatar>
                     </div>
-                    
-                    {userData?.Zupass && userData.Zupass.length > 0 && (
-                        <div className="mt-2 text-sm text-zinc-600">
-                            <p>Member of {userData.Zupass.map((item: { group: string }) => item.group).join(', ')}</p>
+
+                    <div className="flex-grow">
+                        <div className="text-center lg:text-left">
+                            <h2 className="text-2xl font-bold text-zinc-700">{ensName || truncateWallet(recipient)}</h2>
+                            {ensName && <p className="mt-2 font-semibold text-zinc-700">{truncateWallet(recipient)}</p>}
+                            {/* <p className="mt-2 font-semibold text-zinc-700">@{(ensName || truncateWallet(recipient)).toLowerCase()}</p> */}
+                            
+                            {userData?.name && <p className="mt-2 text-zinc-600">{userData.name}</p>}
+                            
+                            {userData?.bio && <p className="mt-2 text-zinc-500">{userData.bio}</p>}
+                            
+                            {userData?.createdAt && (
+                                <p className="mt-2 text-sm text-zinc-400">
+                                    Member since: {new Date(userData.createdAt).toLocaleDateString()}
+                                </p>
+                            )}
+                            
+                            <div className="mt-2 flex justify-center lg:justify-start space-x-4">
+                                {userData?.twitter && (
+                                    <a href={`https://twitter.com/${userData.twitter}`} target="_blank" rel="noopener noreferrer" className="flex items-center text-sky-400">
+                                        <Image src={TwitterLogo} alt="Twitter" className="w-5 h-5 mr-1" />
+                                        @{userData.twitter}
+                                    </a>
+                                )}
+                                {userData?.farcaster && (
+                                    <a href={`https://warpcast.com/${userData.farcaster}`} target="_blank" rel="noopener noreferrer" className="flex items-center text-[#8a63d2]">
+                                        <Image src={FarcasterLogo} alt="Farcaster" className="w-5 h-5 mr-1" />
+                                        @{userData.farcaster}
+                                    </a>
+                                )}
+                            </div>
+                            
+                            {userData?.Zupass && userData.Zupass.length > 0 && (
+                                <div className="mt-2 text-sm text-zinc-600">
+                                    <p>Member of {userData.Zupass.map((item: { group: string }) => item.group).join(', ')}</p>
+                                </div>
+                            )}
+                            
+                            {userData?.rankScore !== undefined && (
+                                <p className="mt-2 font-semibold text-zinc-700">Trust Score: {userData.rankScore}</p>
+                            )}
                         </div>
-                    )}
-                    
-                    {userData?.rankScore !== undefined && (
-                        <p className="mt-2 font-semibold text-zinc-700">Trust Score: {userData.rankScore}</p>
-                    )}
-                </div>
 
-                <div className="mt-6 grid grid-cols-2 gap-6 text-center lg:text-left">
-                    <div>
-                        <p className="font-bold text-zinc-700">{vouchesMade ?? '...'}</p>
-                        <p className="text-sm font-semibold text-zinc-700">Vouches made</p>
+                        <div className="mt-6 grid grid-cols-2 gap-6 text-center lg:text-left">
+                            <div>
+                                <p className="font-bold text-zinc-700">{vouchesMade ?? '...'}</p>
+                                <p className="text-sm font-semibold text-zinc-700">Vouches made</p>
+                            </div>
+
+                            <div>
+                                <p className="font-bold text-zinc-700">{vouchesReceived ?? '...'}</p>
+                                <p className="text-sm font-semibold text-zinc-700">Vouches received</p>
+                            </div>
+                        </div>
+
+                        <div className="mt-6 grid grid-cols-2 gap-4">
+                            <Button onClick={onVouch} className="w-full">Vouch</Button>
+                            <Button onClick={onCancel} variant="outline" className="w-full">Cancel</Button>
+                        </div>
                     </div>
-
-                    <div>
-                        <p className="font-bold text-zinc-700">{vouchesReceived ?? '...'}</p>
-                        <p className="text-sm font-semibold text-zinc-700">Vouches received</p>
-                    </div>
                 </div>
-
-                <div className="mt-6 grid grid-cols-2 gap-4">
-                    <Button onClick={onVouch} className="w-full">Vouch</Button>
-                    <Button onClick={onCancel} variant="outline" className="w-full">Cancel</Button>
-                </div>
-            </div>
+            )}
         </div>
-    </div>
     );
 };
 
